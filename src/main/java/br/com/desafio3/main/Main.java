69de9af21be590eed67612c81f7a0d5af86171e5
Hugo Loiola <hugoloiola.1@gmail.com>
2024-09-13 10:29:26 -0300

    Removendo Redundacias

--- src/main/java/br/com/desafio3/main/Main.java
+++ src/main/java/br/com/desafio3/main/Main.java
@@ -1,6 +1,5 @@
 package br.com.desafio3.main;
 
-
 import br.com.desafio3.pessoa.Atleta;
 import br.com.desafio3.pessoa.Pessoa;
 
@@ -8,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         Pessoa Hugo = new Pessoa("Hugo", 60, 1.75); // Instanciar Pessoa
-        System.out.println("O IMC da pessoa " + Hugo.getNome() + " é " + Hugo.imcPessoa(Hugo.getPeso(), Hugo.getAltura()));
+        System.out.println("O IMC da Pessoa " + Hugo.getNome() + " é " + String.format("%.2f", Hugo.imcPessoa()));
 
         Atleta Hugostoso = new Atleta("Hugostoso", 65, 1.80, "Natação");// Instanciar Atleta
+        System.out.println("O IMC do Atleta " + Hugostoso.getNome() + " é " + String.format("%.2f", Hugostoso.imcPessoa()));
     }
 }
