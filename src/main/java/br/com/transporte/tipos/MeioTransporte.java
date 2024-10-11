acd8dadd3d5df507f50a34dc4eccb5c284858396
Hugo Loiola <hugoloiola.1@gmail.com>
2024-10-11 12:42:15 -0300

    Getters e Setters

--- src/main/java/br/com/transporte/tipos/MeioTransporte.java
+++ src/main/java/br/com/transporte/tipos/MeioTransporte.java
@@ -4,14 +4,38 @@ import br.com.transporte.contrato.Transporte;
 
 public class MeioTransporte implements Transporte {
 
-    protected int velocidade;
-    protected int peso;
+    protected float velocidade;
+    protected double peso;
     protected int numeroPassageiros;
 
     public MeioTransporte(int velocidade, int peso, int numeroPassageiros) {
         super();
         this.velocidade = velocidade;
         this.peso = peso;
+        this.numeroPassageiros = numeroPassageiros;
+    }
+
+    public float getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(float velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getNumeroPassageiros() {
+        return numeroPassageiros;
+    }
+
+    public void setNumeroPassageiros(int numeroPassageiros) {
         this.numeroPassageiros = numeroPassageiros;
     }
 
