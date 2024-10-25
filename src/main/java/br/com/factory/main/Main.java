0e544bc5081e89a51cee24ba240522fa561a76c6
Hugo Loiola <hugoloiola.1@gmail.com>
2024-10-25 10:50:27 -0300

    Factory feita

--- src/main/java/br/com/factory/main/Main.java
+++ src/main/java/br/com/factory/main/Main.java
@@ -1,7 +1,20 @@
 package br.com.factory.main;
 
+import br.com.factory.veiculo.Carro;
+import br.com.factory.veiculo.Moto;
+import br.com.factory.veiculo.VeiculoFactory;
+
 public class Main {
     public static void main(String[] args) {
+        VeiculoFactory fabrica = new VeiculoFactory();
+
+        Moto moto = (Moto) fabrica.criarVeiculo("moto", "CB650R", 50000);
+        System.out.println("Nome: " + moto.getNome());
+        moto.ligar();
+
+        Carro carro = (Carro) fabrica.criarVeiculo("carro", "Civic", 110000);
+        System.out.println("Nome: " + carro.getNome());
+        moto.ligar();
 
     }
 }
