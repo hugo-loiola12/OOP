781db2560d3528481b0358a84f61d88469147cdb
Hugo Loiola <hugoloiola.1@gmail.com>
2024-10-25 10:01:39 -0300

    NeoEnergia feita

--- src/main/java/br/com/energia/luzes/Luz.java
+++ src/main/java/br/com/energia/luzes/Luz.java
@@ -1,5 +1,20 @@
 package br.com.energia.luzes;
 
-public class Luz {
+import br.com.energia.padrao.InterruptorListener;
 
+public class Luz implements InterruptorListener {
+
+    private boolean acesa = false;
+
+    @Override
+    public void aoLigar() {
+        this.acesa = true;
+        System.out.println("Luz ligada");
+    }
+
+    @Override
+    public void aoDesligar() {
+        this.acesa = false;
+        System.out.println("Luz desligada");
+    }
 }
--- src/main/java/br/com/energia/main/Main.java
+++ src/main/java/br/com/energia/main/Main.java
@@ -4,7 +4,7 @@ import br.com.energia.luzes.Interruptor;
 import br.com.energia.luzes.Luz;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Interruptor objInterruptor = new Interruptor();
 
